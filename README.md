30d1a067bcfe126a5a52bb47fa4db3732644e8e1
David Osemwegie <osahonnn@gmail.com>
2018-04-04 19:01:13 -0700

    home page stuff

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # AffixLab2.0
 This is my COSC 448 project
+
--- php/reg/sidebar.php
+++ php/reg/sidebar.php
@@ -5,17 +5,25 @@
         </div>
         <ul class="list-unstyled components">
             <li id="home"><a href="../home/home.php">Home</a></li>
-            <li id="qs">
-                <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false">Sales and Quotes</a>
-                <ul class="collapse list-unstyled" id="homeSubmenu">
-                    <li><a href="../sales/sales.php">View Sales/Quotes</a></li>
-                    <li><a href="../sales/newQuote.php">Add New Quote/Sale</a></li>
-                </ul>
-            </li>
-            <li id="ei"><a href="../employee/employees.php">Employee Information</a></li>
-            <li id="ci"><a href="../customer/customers.php">Customer Information</a></li>
+            <?php include "../checkIfAdmin.php";
+            if ($isAdmin) {
+
+                ?>
+                <li id="qs">
+                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false">Sales and Quotes</a>
+                    <ul class="collapse list-unstyled" id="homeSubmenu">
+                        <li><a href="../sales/sales.php">View Sales/Quotes</a></li>
+                        <li><a href="../sales/newQuote.php">Add New Quote/Sale</a></li>
+                    </ul>
+                </li>
+                <li id="ei"><a href="../employee/employees.php">Employee Information</a></li>
+                <li id="ci"><a href="../customer/customers.php">Customer Information</a></li>
+                <?php
+            }
+            ?>
             <li>
-                <button class="btn btn-lg btn-primary btn-block" id="logoutButton"><a href="../login/logout.php">Logout</a></button>
+                <button class="btn btn-lg btn-primary btn-block" id="logoutButton"><a
+                            href="../login/logout.php">Logout</a></button>
             </li>
         </ul>
     </nav>
